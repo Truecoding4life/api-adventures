7559449774f5d9b015414319f7bb11fbd8f87dcb
Armand Araujo <armandaraujo@Armands-MacBook-Pro.local>
2023-11-07 13:20:00 -0800

    performed minor edits to select file, including gitignore and home-routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,10 @@
 const router = require('express').Router();
 
-// const postRoutes = require('./post-routes');
-// const commentRoutes = require('./comment-routes');
+
 const apiRoutes = require('./api/index.js')
 const homeRoutes = require('./home-routes')
 
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
-// router.use('/home');
 
 module.exports = router;
