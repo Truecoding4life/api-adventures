7be2484ea89b25b61c9aa8177bf0727aeb004c90
Armand Araujo <armandaraujo@Armands-MacBook-Pro.local>
2023-11-07 14:23:57 -0800

    added logout app.get

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -40,3 +40,13 @@ app.post('/login', async (req, res) => {
 
 
 
+app.get('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.redirect('/login'); 
+  });
+});
+
+
