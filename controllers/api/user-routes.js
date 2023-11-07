6c2f07c0096439d82ff61dec390a490607657982
Armand Araujo <armandaraujo@Armands-MacBook-Pro.local>
2023-11-07 14:19:26 -0800

    Added middleware for app.use

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -3,3 +3,12 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'secretly_key', 
+  resave: false,
+  saveUninitialized: true
+}));
+
