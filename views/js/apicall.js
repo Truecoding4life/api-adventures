3786a24dbb503b0ae2616cee1d62b11fc3708515
Andy <84168944+AZurek17@users.noreply.github.com>
2023-11-08 09:19:13 -0700

    added url random photo query link to apicall

--- views/js/apicall.js
+++ views/js/apicall.js
@@ -1,27 +1,12 @@
-const router = require('express').Router();
 require('dotenv').config();
 const { User } = require('../../models');
 
 api_Key = process.env.API_KEY
 // api_Secret = process.env.API_SECRET
 
-url = 'https://api.unsplash.com/photos/random'
-params = {
-    'query': User.category,
-    'orientation': 'squarish',
-    'count': 1,
-}
+url = `https://api.unsplash.com/photos/random?query=${User.category}&orientation=squarish&client_id=${api_Key}`
 
-headers = {
-    'Authorization': 'Client-ID {api_Key}',
-}
+// url = `https://api.unsplash.com/photos/random?query=map&orientation=squarish&client_id=jJoD--t-YwtUk6AG6cgPNH_tl8JWeW-c6MEmMUCfm_8`
 
-response = router.get(url, headers=headers, params=params)
 
-if response.status_code == 200:
-    data = response.json()
-    photo_url = data[0]['urls']['regular']
-    print('Random Squarish Photo: {photo_url}')
-else:
-    print(response.status_code)
-```
+   
