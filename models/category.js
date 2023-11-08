321b4de4b0e7d58b65df5b20263134267464794d
Master <nghiemxthai@gmail.com>
2023-11-08 14:27:24 -0800

    category js file added

---
+++ models/category.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Project extends Model {}
+
+Project.init(
+  {
+  id:{
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  project_name: {
+    type: DataTypes.STRING,
+    allowNull:false,
+  },
+  project_description: {
+    type: DataTypes.TEXT(1000),
+    allowNull:false,
+  },
+  deployed_url:{
+    type: DataTypes.STRING,
+    allowNull:true,
+    isUrl: true
+  },
+  repo_url:{
+    type: DataTypes.STRING,
+    allowNull:true,
+    isUrl: true
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'user',
+      key: 'id',
+    },
+  },
+  }
+  ,
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: false,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
--- seeds/resourceData.js
+++ seeds/resourceData.js
@@ -1,9 +1,20 @@
-const { Comment } = require('../models');
+const { Resource } = require('../models');
 
-const commentdata = [
-// add comments
+const resourceData = [
+    {
+        title: "BARBECUE CHICKEN BASIL CALZONES",
+        description: "This recipe can be easily doubled. This freezes wonderfully and makes a great meal/snack to have on hand. You can also make these and eat them hot out of the oven. This recipe was created for Ready, Set, Cook! #6",
+        image_url:"",  
+        user_id: "2",
+        category_id:"1"
+    },
+    {
+        username: "andy",
+        email: "andy@google.com",  
+        password: "password"
+    }
 ];
 
-const seedComment = () => Comment.bulkCreate(commentdata);
+const seedResource = () => Resource.bulkCreate(resourceData);
 
-module.exports = seedComment;
+module.exports = seedResource;
