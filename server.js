d8e142d271e552d206e69a04bb0fb5cc77de5fb1
Armand Araujo <armandaraujo@Armands-MacBook-Pro.local>
2023-11-08 14:47:14 -0800

    corrected seqelize.sync in server.js

--- server.js
+++ server.js
@@ -46,6 +46,10 @@ app.use(routes);
 
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => 
+    console.log(
+      `\nServer running on port ${PORT}. Visit http://localhost:${PORT} and create an account!`
+    )
+  );
   });
   
