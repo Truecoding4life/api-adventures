bbe86b9c165307d223ab02345948abc6ea943000
Master <nghiemxthai@gmail.com>
2023-11-08 12:59:35 -0800

    added npm

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
-const User = require('./User');
-const Post = require('./Post');
+const User = require('./user');
+const Post = require('./resouce');
 const Comment = require('./Comment');
 // What else do we need?
 // Add user has many ?
---
+++ models/resouce.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class resource extends Model {}
+
+Post.init(
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1]
+        }
+      },
+    
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+);
+
+module.exports = Post;
